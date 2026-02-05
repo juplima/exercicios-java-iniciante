13d1e348ea54774bf1db28648bda41419a5d0584
Juliana Lima <julianapereiralima2501@gmail.com>
2026-02-05 19:37:32 -0300

    Add files via upload

---
+++ 0704/0704/src/pkg0704/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg0704;
+
+/**
+ *
+ * @author juliana.plima10
+ */
+public class Main {
+ public static void frase () {
+ System.out.println("Imprimindo uma função");
+ }
+ public static void Panqueca (){
+     System.out.println("Meu cachorro");
+ }
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frase ();
+        Panqueca();
+        
+    }
+    
+}
